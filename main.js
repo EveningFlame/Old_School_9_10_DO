d8728ded44657d6a5cfd25433805cab4a2e2825d
flyboyb29 <flyboyb29@gmail.com>
2016-02-03 16:14:24 -0800

    updated
    jump fixed

--- main.js
+++ main.js
@@ -1,15 +1,69 @@
 var sb1 = 0;
 var sb2 = 0;
+var totalDistance = 0;
+var maxX = 0;
+
+var defaultGround = 705;
+var defaultSpeed = 0.1;
+var defaultScroll = 250;
+var defaultJumpHeight = 200;
+var defaultScrollSpeed = 2.5;
 
 var unlocked;
 var bgmove;
 
 var standLeft;
+// platforms animation
+function AnimationPlatform(image, frameWidth, frameHeight, imageX, imageY, imageScrolling) {
+    this.image = image;
+    this.width = frameWidth;
+    this.height = frameHeight;
+    this.imageX = imageX;
+    this.imageY = imageY;
+    this.scroll = imageScrolling;
+    this.elapsedTime = 0;
+}
 
-function Animation(spriteSheet, skybg, groundbg, platforms, startX, startY, frameWidth, frameHeight, frameDuration, frames, loop, reverse) {
-    this.platforms = platforms; 
-    this.skybg = skybg;
-    this.groundbg = groundbg;
+AnimationPlatform.prototype.drawFrame = function (tick, ctx, x, y, scaleBy) {
+    var scaleBy = scaleBy || 1;
+    this.elapsedTime += tick;
+
+    if (this.scroll) {
+        ctx.drawImage(this.image,
+                  sb2, 0,  // source from sheet
+                  this.width, this.height,
+                  this.imageX, this.imageY,
+                  this.width, this.height
+                  );
+
+        ctx.drawImage(this.image,
+                  sb1, 0,  // source from sheet
+                  this.width, this.height,
+                  this.imageX, this.imageY,
+                  this.width, this.height
+                  );
+    } else if (maxX >= 250){
+        ctx.drawImage(this.image,
+            0, 0, this.width, this.height,
+            this.imageX - maxX, this.imageY,
+            this.width, this.height);
+    } else {
+        ctx.drawImage(this.image,
+            0, 0, this.width, this.height,
+            this.imageX, this.imageY,
+            this.width, this.height);
+    }
+}
+AnimationPlatform.prototype.currentFrame = function () {
+    return Math.floor(this.elapsedTime / this.frameDuration);
+}
+
+AnimationPlatform.prototype.isDone = function () {
+    return (this.elapsedTime >= this.totalTime);
+}
+
+// sprites animation
+function AnimationSprite(spriteSheet, startX, startY, frameWidth, frameHeight, frameDuration, frames, loop, reverse) {
     this.spriteSheet = spriteSheet;
     this.startX = startX;
     this.startY = startY;
@@ -23,7 +77,7 @@ function Animation(spriteSheet, skybg, groundbg, platforms, startX, startY, fram
     this.reverse = reverse;
 }
 
-Animation.prototype.drawFrame = function (tick, ctx, x, y, scaleBy) {
+AnimationSprite.prototype.drawFrame = function (tick, ctx, x, y, scaleBy) {
     var scaleBy = scaleBy || 1;
     this.elapsedTime += tick;
     if (this.loop) {
@@ -47,100 +101,87 @@ Animation.prototype.drawFrame = function (tick, ctx, x, y, scaleBy) {
     var locX = x;
     var locY = y;
     var offset = vindex === 0 ? this.startX : 0;
-    ctx.drawImage(this.skybg,
-                  0 , 0,  // source from sheet
-                  1200, 800,
-                  -sb2 + 1200 , 0,
-                  1200, 800);
-
-    ctx.drawImage(this.skybg,
-                  0 , 0,  // source from sheet
-                  1200, 800,
-                  -sb1 ,0,
-                  1200, 800);
-
-    // ctx.drawImage(this.groundbg,
-    //               0, -500,  // source from sheet
-    //               1000, 800,
-    //               -sb2 + 1000, 0,
-    //               1000,
-    //               800);
-
-    // ctx.drawImage(this.groundbg,
-    //               0, -500,  // source from sheet
-    //               1000, 800,
-    //               -sb1 ,0,
-    //               //-sb, 0,
-    //               1000,
-    //               800);
-
-    var randplatx = -400;
-    var randplaty = -450;
-
-    // ctx.drawImage(this.platforms,
-    //               randplatx, randplaty,  // source from sheet
-    //               191 - randplatx, 31 - randplaty,
-    //               -sb1 ,0,
-    //               //-sb, 0,
-    //               191 - randplatx,
-    //               31 - randplaty);
-
 
     ctx.drawImage(this.spriteSheet,
-                index * this.frameWidth + offset, vindex * this.frameHeight + this.startY,  // source from sheet
-                this.frameWidth, this.frameHeight,
-                locX, locY,
-                this.frameWidth * scaleBy,
-                this.frameHeight * scaleBy);
-
-
+            index * this.frameWidth + offset, vindex * this.frameHeight + this.startY,  // source from sheet
+            this.frameWidth, this.frameHeight,
+            locX, locY,
+            this.frameWidth * scaleBy,
+            this.frameHeight * scaleBy);
 }
 
-function randoPlatform(ctx) {
-    var randplatx = -20;
-    var randplaty = -20;
-
-    ctx.drawImage(this.platforms,
-                  randplatx, randplaty,  // source from sheet
-                  191 - randplatx, 31 - randplaty,
-                  -sb1 ,0,
-                  //-sb, 0,
-                  191 - randplatx,
-                  31 - randplaty);
-}
-
-Animation.prototype.currentFrame = function () {
+AnimationSprite.prototype.currentFrame = function () {
     return Math.floor(this.elapsedTime / this.frameDuration);
 }
 
-Animation.prototype.isDone = function () {
+AnimationSprite.prototype.isDone = function () {
     return (this.elapsedTime >= this.totalTime);
+ }
+
+function Platform(game, platformSprite, width, height, startX, startY, scroll) {
+    this.animation = new AnimationPlatform(platformSprite, width, height, startX, startY, scroll);
+    this.startX = startX;
+    this.startY = startY;
+    this.width = width;
+    this.height = height;
+    Entity.call(this, game, startX, startY);
+    this.radius = height / 2;
 }
 
-function Background(game) {
-    Entity.call(this, game, 0, 400);
-    this.radius = 200;
+Platform.prototype = new Entity();
+
+Platform.prototype.constructor = Platform;
+
+Platform.prototype.beginingX = function () {
+    return this.startX - maxX;
 }
 
-Background.prototype = new Entity();
-Background.prototype.constructor = Background;
-
-Background.prototype.update = function () {
+Platform.prototype.endingX = function () {
+    return this.startY + this.width - maxX;
 }
 
-Background.prototype.draw = function (ctx) {
+Platform.prototype.top = function () {
+    return this.startY;
+}
+
+Platform.prototype.bottom = function () {
+    return this.startY + this.height;
+}
+
+Platform.prototype.update = function () {
+    Entity.prototype.update.call(this);
+}
+
+Platform.prototype.draw = function (ctx) {
+    this.animation.drawFrame(this.game.clockTick, ctx, 0, 0, 0);
     Entity.prototype.draw.call(this);
 }
-
-function Hero(game, heroSprite, backImage, groundImage) {
-    this.animation = new Animation(heroSprite, backImage, groundImage, ASSET_MANAGER.getAsset("./img/platform.png"), 0, 0.096, 48, 47.004, 0.15 , 12, true, false);
-    this.jumpAnimation = new Animation(heroSprite, ASSET_MANAGER.getAsset("./img/skybg.png"), ASSET_MANAGER.getAsset("./img/groundbg.png"), ASSET_MANAGER.getAsset("./img/platform.png"), 0, 96.096, 48, 47.004, 0.1, 6, false, false);
-    this.rightWalkAnimation = new Animation(heroSprite, ASSET_MANAGER.getAsset("./img/skybg.png"), ASSET_MANAGER.getAsset("./img/groundbg.png"), ASSET_MANAGER.getAsset("./img/platform.png"), 0, 288.096, 48, 47.004, 0.1, 8, true, false);
-    this.animationStandLeft = new Animation(heroSprite, ASSET_MANAGER.getAsset("./img/skybg.png"), ASSET_MANAGER.getAsset("./img/groundbg.png"), ASSET_MANAGER.getAsset("./img/platform.png"), 0, 48.096, 48, 47.004, 0.15, 12, true, true);
-    this.jumpAnimationLeft = new Animation(heroSprite, ASSET_MANAGER.getAsset("./img/skybg.png"), ASSET_MANAGER.getAsset("./img/groundbg.png"), ASSET_MANAGER.getAsset("./img/platform.png"), 0, 144.096, 48, 47.004, 0.1, 6, false, true);
-    this.leftWalkAnimation = new Animation(heroSprite, ASSET_MANAGER.getAsset("./img/skybg.png"), ASSET_MANAGER.getAsset("./img/groundbg.png"), ASSET_MANAGER.getAsset("./img/platform.png"), 0, 336.096, 48, 47.004, 0.1, 8, true, true);   
-    this.radius = 100; 
-    this.ground = 630;
+/*
+* Hero
+* Game: the gameEngine that it will use
+* heroSprite: The sprite sheet for the hero you want to use.
+* standAnimation: the amount of animations in the hero's standing movement
+* walkAnimation: the amount of animations in the hero's walking movement
+* jumpAnimation: the amount of animations in the hero's jumping movement
+*/
+function Hero(game, heroSprite, frameWidth, frameHeight, startX, startY, charYOffset, heroHeight, standAnimation, walkAnimation, jumpAnimation) {
+    this.animation = new AnimationSprite(heroSprite, startX, (startY * 0) + charYOffset,
+        frameWidth, frameHeight - charYOffset, defaultSpeed, standAnimation, true, false);
+    this.jumpAnimation = new AnimationSprite(heroSprite, startX, (startY * 2) + charYOffset,
+        frameWidth, frameHeight - charYOffset, defaultSpeed, jumpAnimation, false, false);
+    this.rightWalkAnimation = new AnimationSprite(heroSprite, startX, (startY * 6) + charYOffset,
+        frameWidth, frameHeight - charYOffset, defaultSpeed, walkAnimation, true, false);
+    this.animationStandLeft = new AnimationSprite(heroSprite, startX, startY + charYOffset,
+        frameWidth, frameHeight - charYOffset, defaultSpeed, standAnimation, true, true);
+    this.jumpAnimationLeft = new AnimationSprite(heroSprite, startX, (startY * 3) + charYOffset,
+        frameWidth, frameHeight - charYOffset, defaultSpeed, jumpAnimation, false, true);
+    this.leftWalkAnimation = new AnimationSprite(heroSprite, startX, (startY * 7) + charYOffset,
+        frameWidth, frameHeight - charYOffset, defaultSpeed, walkAnimation, true, true);
+    this.radius = 100;
+    this.heroHeight = heroHeight;
+    this.ground = defaultGround - heroHeight;
+    this.scrollSpeed = defaultScrollSpeed;
+    this.jumpHeight = defaultJumpHeight;
     Entity.call(this, game, 0, 400);
 }
 
@@ -148,6 +189,12 @@ Hero.prototype = new Entity();
 Hero.prototype.constructor = Hero;
 
 Hero.prototype.update = function () {
+    console.log("total distance = " + totalDistance);
+    this.jumpHeight = underPlatform(this.x, this.game);
+    if (this.jumpHeight < 0) {
+        this.jumpHeight = defaultJumpHeight;
+    }
+
     if (this.game.space) this.jumping = true;
     if (this.jumping) {
         if (this.jumpAnimation.isDone()) {
@@ -155,12 +202,11 @@ Hero.prototype.update = function () {
             this.jumping = false;
         }
         var jumpDistance = this.jumpAnimation.elapsedTime / this.jumpAnimation.totalTime;
-        var totalHeight = 100;
+        var totalHeight = this.jumpHeight;
 
         if (jumpDistance > 0.5)
             jumpDistance = 1 - jumpDistance;
 
-        //var height = jumpDistance * 2 * totalHeight;
         var height = totalHeight*(-4 * (jumpDistance * jumpDistance - jumpDistance));
         this.y = this.ground - height;
     }
@@ -171,28 +217,26 @@ Hero.prototype.update = function () {
             this.jumping = false;
         }
         var jumpDistance = this.jumpAnimationLeft.elapsedTime / this.jumpAnimationLeft.totalTime;
-        var totalHeight = 100;
+        var totalHeight = this.jumpHeight;
 
         if (jumpDistance > 0.5)
             jumpDistance = 1 - jumpDistance;
 
-        //var height = jumpDistance * 2 * totalHeight;
         var height = totalHeight*(-4 * (jumpDistance * jumpDistance - jumpDistance));
         this.y = this.ground - height;
     }
     
-
     if (bgmove && this.game.walkRight) { 
-        sb1 += 2;           // background movement lock
-        sb2 += 2;
+        sb1 += this.scrollSpeed;           // background movement lock
+        sb2 += this.scrollSpeed;
     }
 
     if (unlocked && this.game.walkRight) {
-        this.x += 2;
+        this.x += this.scrollSpeed;
     }
 
     if (unlocked && this.game.walkLeft) {
-        this.x -= 2;
+        this.x -= this.scrollSpeed;
     }
 
     console.log("sb1: " + sb1 + " sb2: " + sb2 + " x: " + this.x);
@@ -201,17 +245,18 @@ Hero.prototype.update = function () {
 }
 
 Hero.prototype.draw = function (ctx) {
-    if (sb1 > 1200) {
+	var yPlace = this.ground;
+    if (sb1 > 2400) {
         sb1 = 0;
     }
-    if (sb2 > 1200) {
+    if (sb2 > 2400) {
         sb2 = 0;
     }
-    if (this.x >= 500) {
+    if (this.x >= defaultScroll) {
         unlocked = false;
         bgmove = true;
     }
-    if (this.x < 500) {
+    if (this.x < defaultScroll || totalDistance > 4600) {
         unlocked = true;
         bgmove = false;
     }
@@ -222,36 +267,90 @@ Hero.prototype.draw = function (ctx) {
         unlocked = false;
     }
 
-    // if (standLeft) {
-    //     unlocked = true;
-    // } 
-    // if (this.x < 5 && this.game.walkRight) {
-    //     unlocked = false;
-    // }
-
     if (this.jumping) {
         if (standLeft) {
-            this.jumpAnimationLeft.drawFrame(this.game.clockTick, ctx, this.x, this.y, 1.6);
+            this.jumpAnimationLeft.drawFrame(this.game.clockTick, ctx, this.x, this.y, 2);
+            if (this.game.walkLeft) if (unlocked) totalDistance -= this.scrollSpeed;
         } else {
-            this.jumpAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y, 1.6);
+            this.jumpAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y, 2);
+            if (this.game.walkRight) {
+                totalDistance += this.scrollSpeed;
+                if (totalDistance > maxX) maxX += this.scrollSpeed;
+            }
         }
-    }    
-    else if (this.game.walkRight) {
+    } else if (this.game.walkRight) {
         standLeft = false;
-        this.rightWalkAnimation.drawFrame(this.game.clockTick, ctx, this.x, 630, 1.6);
-
+        this.rightWalkAnimation.drawFrame(this.game.clockTick, ctx, this.x, yPlace, 2);
+        totalDistance += this.scrollSpeed;
+        if (totalDistance > maxX) maxX += this.scrollSpeed;
     } else if (this.game.walkLeft) {
         standLeft = true;
-        this.leftWalkAnimation.drawFrame(this.game.clockTick, ctx, this.x, 630, 1.6);
-    }
-    else {
+        this.leftWalkAnimation.drawFrame(this.game.clockTick, ctx, this.x, yPlace, 2);
+        if (unlocked) totalDistance -= this.scrollSpeed;
+    } else {
         if (standLeft) {
-            this.animationStandLeft.drawFrame(this.game.clockTick, ctx, this.x, 630, 1.6);
+            this.animationStandLeft.drawFrame(this.game.clockTick, ctx, this.x, yPlace, 2);
         } else {
-            this.animation.drawFrame(this.game.clockTick, ctx, this.x, 630, 1.6);
+            this.animation.drawFrame(this.game.clockTick, ctx, this.x, yPlace, 2);
         }
     }
 
+    Entity.prototype.draw.call(this);
+}
+
+function underPlatform(currentX, game) {
+    var newHeight = -1;
+    //if (currentX >= game.p1.beginingX() && currentX <= game.p1.endingX()) {
+    //    newHeight = defaultGround - game.p1.bottom();
+    //}
+    return newHeight;
+}
+/*
+ * Minions
+ */
+function Minion(game, minionSprite, frameHeight, frameWidth, startX, startY,
+    walking1, placeX, placeY, loop) {
+
+    this.animationWalkingLeft1 = new AnimationSprite(minionSprite, startX, (startY * 0),
+        frameWidth, frameHeight, defaultSpeed, walking1, loop, false);
+    this.animationWalkingRight1 = new AnimationSprite(minionSprite, startX, (startY * 1),
+        frameWidth, frameHeight, defaultSpeed, walking1, loop, false);
+
+    //if (walking2 > 0) {
+    //    this.animationWalkingLeft2 = new AnimationSprite(minionSprite, startX, (startY * 5),
+    //    frameWidth, frameHeight, defaultSpeed, walking2, loop, false);
+    //    this.animationWalkingRight2 = new AnimationSprite(minionSprite, startX, (startY * 6),
+    //        frameWidth, frameHeight, defaultSpeed, walking2, loop, false);
+    //}
+    
+    this.radius = frameHeight / 2;
+    this.y = placeY;
+    this.x = placeX;
+    this.moveRight = false;
+    Entity.call(this, game, placeX, placeY);
+}
+
+Minion.prototype = new Entity();
+
+Minion.prototype.constructor = Minion;
+
+Minion.prototype.update = function () {
+
+    if (this.moveRight) {
+        this.x += defaultScrollSpeed;
+    } else {
+        this.x -= defaultScrollSpeed;
+    }
+    Entity.prototype.update.call(this);
+}
+
+Minion.prototype.draw = function (ctx) {
+
+    if (this.moveRight) {
+        this.animationWalkingRight1.drawFrame(this.game.clockTick, ctx, this.x, this.y, 2);
+    } else {
+        this.animationWalkingLeft1.drawFrame(this.game.clockTick, ctx, this.x, this.y, 2);
+    }
     Entity.prototype.draw.call(this);
 }
 
@@ -259,31 +358,44 @@ Hero.prototype.draw = function (ctx) {
 
 var ASSET_MANAGER = new AssetManager();
 
-ASSET_MANAGER.queueDownload("./img/RobotUnicorn.png");
-ASSET_MANAGER.queueDownload("./img/mariosprite.png");
 ASSET_MANAGER.queueDownload("./img/mariosprite2.png");
-ASSET_MANAGER.queueDownload("./img/mariospriteleft.png");
-ASSET_MANAGER.queueDownload("./img/skybg.png");
-ASSET_MANAGER.queueDownload("./img/groundbg.png");
+ASSET_MANAGER.queueDownload("./img/skybg2.png");
+ASSET_MANAGER.queueDownload("./img/groundbg2.png");
 ASSET_MANAGER.queueDownload("./img/platform.png");
-
+ASSET_MANAGER.queueDownload("./img/koopa2.png");
 
 ASSET_MANAGER.downloadAll(function () {
     console.log("starting up da sheild");
     var canvas = document.getElementById('gameWorld');
     var ctx = canvas.getContext('2d');
-
-    var mario = ASSET_MANAGER.getAsset("./img/mariosprite2.png");
-    var world1 = ASSET_MANAGER.getAsset("./img/skybg.png");
-    var ground1 = ASSET_MANAGER.getAsset("./img/groundbg.png");
+    var marioSprite = ASSET_MANAGER.getAsset("./img/mariosprite2.png");
+    var world1Sprite = ASSET_MANAGER.getAsset("./img/skybg2.png");
+    var ground1 = ASSET_MANAGER.getAsset("./img/groundbg2.png");
+    var platform1Sprite = ASSET_MANAGER.getAsset("./img/platform.png");
+    var Koopa = ASSET_MANAGER.getAsset("./img/koopa2.png");
 
     var gameEngine = new GameEngine();
-    var bg = new Background(gameEngine);
-    var unicorn = new Hero(gameEngine, mario, world1, ground1);
+    var bg = new Platform(gameEngine, world1Sprite, 800, defaultGround, 0, 0, true);
+    var gr = new Platform(gameEngine, ground1, 800, 95, 0, defaultGround, true);
+    var hero = new Hero(gameEngine, marioSprite, 48, 48, 0, 48, 0.192, 95, 12, 8, 6);
 
+    var p1 = new Platform(gameEngine, platform1Sprite, 191, 31, 1500, 600, false);
+    var p2 = new Platform(gameEngine, platform1Sprite, 191, 31, 1600, 500, false);
+    var b1 = new Platform(gameEngine, platform1Sprite, 31, 31, 1250, 674, false);
+    var b2 = new Platform(gameEngine, platform1Sprite, 31, 31, 2250, 674, false);
+
+    var m1 = new Minion(gameEngine, Koopa, 688, 417, 0, 688, 6, defaultGround, 600, true);
+
+    gameEngine.addEntity(gr);
     gameEngine.addEntity(bg);
-    gameEngine.addEntity(unicorn);
- 
+    gameEngine.addEntity(hero);
+    gameEngine.addEntity(p1);
+    gameEngine.addEntity(p2);
+    gameEngine.addEntity(b1); // put koopa between here and p4
+    gameEngine.addEntity(b2);
+
+    //gameEngine.addEntity(m1);
+
     gameEngine.init(ctx);
     gameEngine.start();
 });
