a32c69a2fe5aa957bed0f2a507e4a697447081af
flyboyb29 <flyboyb29@gmail.com>
2016-02-27 00:41:27 -0800

    j

--- platformAnimation.js
+++ platformAnimation.js
@@ -13,46 +13,18 @@ function AnimationPlatform(image, frameWidth, frameHeight, imageX, imageY, image
 AnimationPlatform.prototype.drawFrame = function (tick, ctx, x, y, scaleBy) {
     var scaleBy = scaleBy || 1;
     this.elapsedTime += tick;
-    
-    
-    // ctx.font = "36px serif";
-    // ctx.strokeText("Score: ", 10, 40);
-    // ctx.strokeText(this.game.score, 110, 40);
-    
-    // if (this.game.poweredUp) {
-    //   //console.log(this.game.starTime);
-    //     this.game.starTime -= this.game.clockTick;
-
-    //     ctx.strokeText("IM SUPER", 300, 200);
-    //     if (this.game.starTime < 0) {
-    //       this.game.poweredUp = false;
-    //       this.game.starTime = 50;
-    //     }
-    // }
 
     if (this.scroll) {
-        // ctx.drawImage(this.image,
-        //           this.game.sb2, 0,  // source from sheet
-        //           this.width, this.height,
-        //           this.imageX, this.imageY,
-        //           this.width, this.height
-        //           );
-
         ctx.drawImage(this.image,
                   this.game.sb1, 0,  // source from sheet
                   this.width, this.height,
                   this.imageX, this.imageY,
                   this.width, this.height
                   );
-    } else if (this.game.maxX >= 250) {
-        ctx.drawImage(this.image,
-            0, 0, this.width, this.height,
-            this.imageX - this.game.maxX, this.imageY,
-            this.width, this.height);
     } else {
         ctx.drawImage(this.image,
             0, 0, this.width, this.height,
-            this.imageX, this.imageY,
+            this.imageX - this.game.maxX, this.imageY,
             this.width, this.height);
     }
 };
